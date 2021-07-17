87671dd1c9a72a8d39d67252543e18922ea8152c
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-07-17 12:32:57 +0530

    add scroll logic

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
     return (
         <div className='app'>
             <Header />
+            <div style={{ backgroundColor: 'red', height: '800px' }}></div>
+            <div style={{ backgroundColor: 'blue', height: '800px' }}></div>
+            <div style={{ backgroundColor: 'green', height: '800px' }}></div>
+            <div style={{ backgroundColor: 'yellow', height: '800px' }}></div>
         </div>
     );
 }
